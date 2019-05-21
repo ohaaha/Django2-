c54ed9cced201577330576b37715439d2985295f
rhd <1258485146@qq.com>
2019-05-21 22:06:19 +0800

    修改views.py

--- HelloWorld/blog/views.py
+++ HelloWorld/blog/views.py
@@ -13,7 +13,7 @@ def index(request):
     # 最新文章
     top4_article = models.Article.objects.order_by('-public_date')[:4] #按时间排序取4个
     # 分页
-    paginator = Paginator(article, 4) # 每页为4
+    paginator = Paginator(article, 5) # 每页为4
     page_num = paginator.num_pages # 获取总共几页
     page_list = paginator.page(page)
     next_page = page+1 if(page_list.has_next()) else page
@@ -28,23 +28,23 @@ def index(request):
 
 def article_page(request, article_id):
     articles = models.Article.objects.all()
-    article = articles[article_id]
-    temp = article.id;
-    if(temp == 0):
-        last_id = 0
+    temp = article_id;
+    if(temp == 1):
+        last_id = 1
         next_id = temp+1
         last_title = "无"
-        next_title = articles[next_id].title
-    elif (temp == len(articles)-1):
+        next_title = articles[next_id-1].title
+    elif (temp == len(articles)):
         last_id = temp-1
         next_id = temp
-        last_title = articles[last_id].title
+        last_title = articles[last_id-1].title
         next_title = "无"
     else:
         last_id = temp-1
         next_id = temp+1
-        last_title = articles[last_id].title
-        next_title = articles[next_id].title
+        last_title = articles[last_id-1].title
+        next_title = articles[next_id-1].title
+    article = articles[article_id - 1]
     section_list = article.content.split('\n')  # 把文章内容用换行符隔开
     return render(request, 'blog/article_page.html', {'article': article,
                                                       'section_list': section_list,
